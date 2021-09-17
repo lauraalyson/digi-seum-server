0bd4365be085868ac69046949a7100339d0e5c92
lauraalyson <lauraalyson3@gmail.com>
2021-09-17 19:43:35 -0400

    add artwork schema for reset

---
+++ app/models/artwork.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const artworkSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  img: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Artwork', artworkSchema)
